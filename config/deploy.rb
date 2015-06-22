5b4e7f3eccd8c8d8ff58e44471fd56a092d83b3c
alexeyShumakov <bobshaftoe@yandex.ru>
2015-06-22 20:36:33 +0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,10 @@ set :scm, :git
 set :format, :pretty
 set :log_level, :debug
 set :pty, true
-set :rvm_type, :user
+# set :rvm_type, :system
 set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
