f65ef9e455fe5202c1f88136ab62fb57501f2989
alexeyShumakov <bobshaftoe@yandex.ru>
2015-06-30 15:21:51 +0500

    nice working deploy script! yay!

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 set :application, 'putmash'
 set :repo_url, 'https://github.com/alexeyShumakov/putmash.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :deploy_to, '/home/alexey/putmash'
 set :scm, :git
@@ -11,12 +10,12 @@ set :log_level, :debug
 set :pty, true
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.2'
+set :unicorn_config_path, 'config/unicorn.rb'
 
 set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
-# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
 namespace :deploy do
   task :setup do
     before "deploy:migrate", :create_db
@@ -32,26 +31,10 @@ namespace :deploy do
       end
     end
   end
-  task :update_crontab do
-    desc 'Update crontab'
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
+
   task :restart do
-    desc "Restart Unicorn"
-    on roles(:all) do
-       sudo "service unicorn restart"
-    end
+    invoke 'unicorn:legacy_restart'
   end
 
-  after :restart, :clear_cache do
-    on roles(:all) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, 'cache:clear'
-        end
-      end
-    end
-  end
-
-  before "deploy:restart", "deploy:update_crontab"
+  after 'deploy:publishing', 'deploy:restart'
 end
